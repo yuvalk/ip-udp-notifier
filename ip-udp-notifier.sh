1d93e3cfe64e2fd2742e0e766886dba7af8762d3
Yuval Kashtan <yuvalkashtan@gmail.com>
2024-03-20 16:15:12 +0200

    add hostname to broadcasted data

--- ip-udp-notifier.sh
+++ ip-udp-notifier.sh
@@ -3,6 +3,6 @@
 while [ 1 ]
 do
 	echo "sending ips to udp 10.46.97.97:3000"
-	echo $(hostname -I) > /dev/udp/10.46.97.97/3000
+	echo $(hostname)=$(hostname -I) > /dev/udp/10.46.97.97/3000
 	sleep 5s
 done
